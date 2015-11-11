feedaa4dc8852a904d661dc84ab8797e73f5c4b7
blyjng <b@ileyjiang.com>
2015-11-11 16:33:52 -0700

    initial creation

---
+++ gui/timeline.h
@@ -0,0 +1,17 @@
+#ifndef TIMELINE_H
+#define TIMELINE_H
+
+#include <QWidget>
+
+class Timeline : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Timeline(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // TIMELINE_H
